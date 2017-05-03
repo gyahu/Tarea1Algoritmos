60be5018a4654e4261f8cd027d75bce12ce4c75d
gyahu <tpvchile@gmail.com>
2017-05-03 02:08:21 -0300

    Moved to sort() method to recycle code.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,51 @@
 using namespace std;
 
 /* Moves the different values to the corresponding bucket */
-void redirect(string batch, std::vector<int> pivots, std::vector<FILE *> buckets){
+void redirect(string batch, std::vector<int> pivots, std::vector<FILE *> buckets, int index){
 
+}
+
+/* Sorts the file using pivotAmmount pivots and acording to the index */
+void sort(FILE * data, int pivotAmmount, int index){
+
+	/* Variables per sort */
+	std::vector<int> pivots(pivotAmmount, 0);
+	std::vector<FILE *> buckets(pivotAmmount+1);
+	string buffer;
+  	fpos_t position;
+	int counter = 0;
+
+	buffer = (char *) malloc(size);
+  	fgetpos (pFile, &position);
+
+  	/* Get pivots */
+  	while (counter < pivotAmmount){
+
+	  	/* Move data to main memory */
+	  	result = fread(buffer, 1, size, pFile);
+
+	  	// pivots[counter] = value;
+
+	  	++counter;
+  	}
+
+  	/* Open files for each bucket */
+  	for (int i = 0; i < pivotAmmount; ++i){
+  		buckets[i] = fopen("bucket"+to_string(i), "a+");
+  	}
+
+  	/* Restart the file position */
+  	fsetpos (pFile, &position);
+
+  	/* Move data to buckets by batches */
+  	while (fread(buffer, 1, size, pFile)){
+  		redirect(buffer, pivots, buckets, index);
+  	}
+
+  	/* CLOSE */
+  	for (int i = 0; i < pivotAmmount; ++i){
+  		fclose(buckets[i]);
+  	}
 }
 
 int main(){
@@ -18,58 +61,16 @@ int main(){
 
 	/* Iterate for the different possible data sizes */
 	for (int i = 9; i < 22; ++i){
-
-		/* Variables per data */
-		std::vector<int> pivots(pivotAmmount, 0);
-		std::vector<FILE *> buckets(pivotAmmount+1);
-
-		/* Open the folder of the corresponding data */
-		string dir_path("folder_2**"+to_string(i));
-		boost::filesystem::path dir(dir_path);
-		boost::filesystem::create_directory(dir);
+		FILE * pFile;
 
 		/* Open raw data */
-		FILE * pFile;
-  		fpos_t position;
-		string buffer;
-	  	pFile = fopen ("raw_data.txt", "r");
-  		fgetpos (pFile, &position);
-		buffer = (char *) malloc(size);
+		string dir_path("folder_2**"+to_string(i));
+	  	pFile = fopen (dir_path+"raw_data.txt", "r");
 
-	  	/* SORT */
-
-		int counter = 0;
-	  	/* Get pivots */
-	  	while (counter < pivotAmmount){
-
-		  	/* Move data to main memory */
-		  	result = fread(buffer, 1, size, pFile);
-
-		  	// pivots[counter] = value;
-
-		  	++counter;
-	  	}
-
-	  	/* Open files for each bucket */
-	  	for (int i = 0; i < pivotAmmount; ++i){
-	  		buckets[i] = fopen("bucket"+to_string(i), "a+");
-	  	}
-
-	  	/* Restart the file */
-	  	fsetpos (pFile, &position);
-
-	  	/* Move data to buckets by batches */
-	  	while (fread(buffer, 1, size, pFile)){
-	  		redirect(buffer, pivots, buckets);
-	  	}
-
-
+		sort(pFile, pivotAmmount, 1);
+		sort(pFile, pivotAmmount, 2);
+			
 	  	/* CLOSE */
-
-	  	for (int i = 0; i < pivotAmmount; ++i){
-	  		fclose(buckets[i]);
-	  	}
-
 	  	fclose(pFile)
 	}
 	return 0;
